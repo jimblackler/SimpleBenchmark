1686f44d82f6e358cbf7fd59a9833e7d65c4bf4e
Jim Blackler <jimblackler@google.com>
2021-10-16 18:27:46 +0100

    Iterations per second.

--- app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
+++ app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
@@ -1,24 +1,39 @@
 package net.jimblackler.simplebenchmark;
 
+import static java.lang.System.nanoTime;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Choreographer;
 import androidx.databinding.DataBindingUtil;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
 import net.jimblackler.simplebenchmark.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
+  private static final long NANOSECONDS_PER_SECOND = 1000000000;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    AtomicInteger a = new AtomicInteger();
+    ArrayList<Long> times = new ArrayList<>();
+
     super.onCreate(savedInstanceState);
     ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    new Thread(() -> {
+      while (true) {
+        long time = nanoTime();
+        while (!times.isEmpty() && times.get(0) < time - NANOSECONDS_PER_SECOND) {
+          times.remove(0);
+        }
+        times.add(time);
+      }
+    }).start();
 
     Choreographer instance = Choreographer.getInstance();
     Choreographer.FrameCallback callback = new Choreographer.FrameCallback() {
       @Override
       public void doFrame(long frameTimeNanos) {
-        binding.progress.setText("Update: " + a.incrementAndGet());
+        binding.progress.setText("Iterations per second: " + times.size());
         instance.postFrameCallback(this);
       }
     };
