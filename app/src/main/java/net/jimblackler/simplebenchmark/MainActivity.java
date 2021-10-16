988291395ba86e336bd0912ada01e91d6e5a9feb
Jim Blackler <jimblackler@google.com>
2021-10-16 17:11:11 +0100

    Test display.

--- app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
+++ app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
@@ -1,12 +1,27 @@
 package net.jimblackler.simplebenchmark;
 
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.Choreographer;
+import androidx.databinding.DataBindingUtil;
+import java.util.concurrent.atomic.AtomicInteger;
+import net.jimblackler.simplebenchmark.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    AtomicInteger a = new AtomicInteger();
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    Choreographer instance = Choreographer.getInstance();
+    Choreographer.FrameCallback callback = new Choreographer.FrameCallback() {
+      @Override
+      public void doFrame(long frameTimeNanos) {
+        binding.progress.setText("Update: " + a.incrementAndGet());
+        instance.postFrameCallback(this);
+      }
+    };
+    instance.postFrameCallback(callback);
   }
 }
