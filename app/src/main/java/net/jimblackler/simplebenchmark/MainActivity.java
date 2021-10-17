e88ed4e4fd3ed10b351c901aec5bd1a6119bded4
Jim Blackler <jimblackler@google.com>
2021-10-17 09:49:59 +0100

    Use milliseconds not nanoseconds. Also change data structure.

--- app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
+++ app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
@@ -1,29 +1,30 @@
 package net.jimblackler.simplebenchmark;
 
-import static java.lang.System.nanoTime;
+import static java.lang.System.currentTimeMillis;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Choreographer;
 import androidx.databinding.DataBindingUtil;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import net.jimblackler.simplebenchmark.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
-  private static final long NANOSECONDS_PER_SECOND = 1000000000;
+  private static final long MILLISECONDS_PER_SECOND = 1000;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    ArrayList<Long> times = new ArrayList<>();
+    LinkedList<Long> times = new LinkedList<>();
 
     super.onCreate(savedInstanceState);
     ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     new Thread(() -> {
       while (true) {
-        long time = nanoTime();
-        while (!times.isEmpty() && times.get(0) < time - NANOSECONDS_PER_SECOND) {
-          times.remove(0);
+        long time = currentTimeMillis();
+
+        while (!times.isEmpty() && times.getFirst() < time - MILLISECONDS_PER_SECOND) {
+          times.removeFirst();
         }
         times.add(time);
       }
