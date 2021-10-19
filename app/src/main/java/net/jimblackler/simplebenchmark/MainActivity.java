49b769d334a0cfa4e5911291f923a0d1820a5f19
Jim Blackler <jimblackler@google.com>
2021-10-19 17:09:13 +0100

    Experimental boost mode.

--- app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
+++ app/src/main/java/net/jimblackler/simplebenchmark/MainActivity.java
@@ -4,9 +4,12 @@ import static java.lang.System.nanoTime;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PerformanceHintManager;
 import android.os.Process;
+import android.util.Log;
 import android.view.Choreographer;
 import androidx.annotation.RequiresApi;
 import androidx.databinding.DataBindingUtil;
@@ -14,10 +17,15 @@ import java.util.concurrent.atomic.AtomicLong;
 import net.jimblackler.simplebenchmark.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
-  private static final int NUMBER_ITERATIONS = 100000;
-  private static final int NANOSECONDS_PER_MICROSECOND = 1000;
+  private static final String TAG = MainActivity.class.getSimpleName();
 
-  @RequiresApi(api = Build.VERSION_CODES.N)
+  private static final int NUMBER_ITERATIONS = 100000;
+  private static final int MICROSECONDS_PER_SECOND = 1000000;
+  private static final int NANOSECONDS_PER_MICROSECOND = 1000;
+  private static final long NANOSECONDS_PER_SECOND =
+      MICROSECONDS_PER_SECOND * NANOSECONDS_PER_MICROSECOND;
+
+  @RequiresApi(api = Build.VERSION_CODES.S)
   @SuppressLint("SetTextI18n")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -50,5 +58,15 @@ public class MainActivity extends Activity {
     };
     instance.postFrameCallback(callback);
     binding.processPid.setText("Process pid: " + Process.myPid());
+
+    binding.boost.setOnClickListener(v -> {
+      @SuppressLint("WrongConstant")
+      PerformanceHintManager manager =
+          (PerformanceHintManager) getSystemService(Context.PERFORMANCE_HINT_SERVICE);
+      int[] tids = {Process.myPid()};
+      PerformanceHintManager.Session hintSession =
+          manager.createHintSession(tids, 5 * NANOSECONDS_PER_SECOND);
+      Log.i(TAG, hintSession == null ? "no" : "yes");
+    });
   }
 }
